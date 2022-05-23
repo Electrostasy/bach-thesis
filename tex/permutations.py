90a4dada4d25b6653de3217fd177dbe6d3c1fe1b
Gediminas Valys <steamykins@gmail.com>
2022-05-23 14:30:08 +0300

    update

--- tex/permutations.py
+++ tex/permutations.py
@@ -1,6 +1,5 @@
 from itertools import permutations
 from string import printable
 
-for i in range(1, 7):
-  for c in permutations(printable[0:-5], i):
-    print(''.join(c))
+for c in permutations(printable[0:-5], 5):
+  print(''.join(c))
